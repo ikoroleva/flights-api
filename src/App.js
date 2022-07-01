78b4ae18cd2d6453a642802e54ec19e28d209a84
Ilja M <i.meleshkevich@gmail.com>
2022-07-01 12:02:19 +0200

    added selects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { SearchResults } from "./SearchResults";
 import OurDropDown from "./Dropdown";
 import { DropdownButton, Dropdown } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  console.log(from, to);
   return (
     <div>
-      <OurDropDown />
-      <SearchResults from="PRG" to="VLC" />
+      <OurDropDown setFrom={setFrom} setTo={setTo} />
+      <SearchResults from={from} to={to} />
     </div>
   );
 }
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,31 +1,27 @@
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
 
-const OurDropDown = () => {
-  const [from, setFrom] = useState("");
-  const [to, setTo] = useState("");
-
+const OurDropDown = ({ setFrom, setTo }) => {
   return (
     <div className="select__flight">
       <div className="select__flight_dep">
-        <Form.Select size="lg">
-          <option>Prague</option>
-          <option>Berlin</option>
-          <option>Warsaw</option>
-          <option>Pardubice</option>
+        <Form.Select size="lg" onChange={(e) => setFrom(e.target.value)}>
+          <option value="PRG">Prague</option>
+          <option value="BER">Berlin</option>
+          <option value="WAW">Warsaw</option>
+          <option value="PED">Pardubice</option>
         </Form.Select>
       </div>
-      <div className="select__flight_from">{from}</div>
+
       <div className="select__flight_arr">
-        <Form.Select size="lg">
-          <option>Valencia</option>
-          <option>Barcelona</option>
-          <option>Madrid</option>
-          <option>Milano</option>
-          <option>Athens</option>
+        <Form.Select size="lg" onChange={(e) => setTo(e.target.value)}>
+          <option value="VLC">Valencia</option>
+          <option value="BCN">Barcelona</option>
+          <option value="MAD">Madrid</option>
+          <option value="MXP">Milano</option>
+          <option value="ATH">Athens</option>
         </Form.Select>
       </div>
-      <div className="select__flight_to"></div>
     </div>
   );
 };
